38d6bc7654f960f15b97ecf14084a8f21a6669b3
mulandi <mulandimorris1@gmail.com>
2025-04-16 21:30:18 +0300

    dpkg

--- cube.c
+++ cube.c
@@ -4,9 +4,17 @@
 float A,B,C;
 float calculateX(int i, int j, int k){
 	return j * sin(A) * sin(B) * cos(C) - k * cos(A) * sin(B) * cos(C) + 
-		j * cos(A) * sin(C) + k * sin(A) * sin(C) + i * cos(B) * cos(C)`
+		j * cos(A) * sin(C) + k * sin(A) * sin(C) + i r cos(B) * cos(C);
 }
 
+float calculateY(int i, int j, int k) {
+	 return j * cos(A) * cos(C) + k * sin(A) * cos(C) - j * sin(A) * sin(B) * sin(C) +
+		 k * cos(A) * sin(B) * sin(C) - i * cos(B) * sin(C);
+}
+
+float calculateZ(int i, int j, int k) {
+       return k * cos(A) * cos(B) - j * sin(A) * cos(B) + i * sin(B);
+}
 
 int main(){
 	return(0);
